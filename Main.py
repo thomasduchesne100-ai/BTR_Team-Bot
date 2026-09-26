b8bb7a81f3f936dca130e32912ab824190462251
Tomate Cerise <thomas_duchesne100@outlook.com>
2026-09-26 12:54:29 -0400

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,1 +1,24 @@
+import discord
+from discord.ext import commands
+import os
 
+intents = discord.Intents.default()
+intents.message_content = True
+intents.members = True
+
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f"Connecté en tant que {bot.user}")
+
+@bot.command()
+async def ping(ctx):
+    await ctx.send(f"Pong ! `{round(bot.latency * 1000)}ms`")
+
+TOKEN = os.getenv("DISCORD_TOKEN")
+
+if not TOKEN:
+    raise ValueError("DISCORD_TOKEN n'est pas configuré.")
+
+bot.run(TOKEN)
